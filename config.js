bd04e7d265571c0dc0f4eac4da7587eae6586718
NARARYA JAYA UTAMA GROUB <nararyajayautamagroub@gmail.com>
2026-09-20 13:16:08 +0700

    chore: version application runtime

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ window.NX_CONFIG={
     redirectTo:window.location.origin+window.location.pathname
   },
   app:{
+    version:"5.2.0",
     defaultLanguage:"id",
     supportedLanguages:["id","en","ms","vi","th","zh","ja","ko","ar","es"],
     autoRefreshMinutes:15
